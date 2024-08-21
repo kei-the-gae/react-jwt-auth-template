85b54ab2cfb809446aaafc2fb140eb0c7bb5b836
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-21 15:01:06 -0700

    Add sign out function to clear local storage and set state

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,14 @@ import Dashboard from './components/Dashboard/Dashboard';
 const App = () => {
   const [user, setUser] = useState(authService.getUser());
 
+  const handleSignout = () => {
+    authService.signout();
+    setUser(null);
+  };
+
   return (
     <>
-      <NavBar user={user} />
+      <NavBar user={user} handleSignout={handleSignout} />
       <Routes>
         {user ? (
           <Route path="/" element={<Dashboard user={user} />} />
